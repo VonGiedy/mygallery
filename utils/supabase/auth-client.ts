8e27a6d90d651bd79fc5a35a6f807af86655d495
vongiedyy <vgbaguilar@addu.edu.ph>
2026-03-09 03:55:33 +0800

    Updated AUTH

--- utils/supabase/auth-client.ts
+++ utils/supabase/auth-client.ts
@@ -1,9 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const { signIn, signUp, useSession, signOut } = createAuthClient({
-  // If we are in the browser, use the current domain (Vercel).
-  // Otherwise, use localhost for local development.
-  baseURL: typeof window !== "undefined" 
-    ? window.location.origin 
-    : "http://localhost:3000",
+  baseURL: "https://mygallery-tan.vercel.app", // Your local development URL
 });
